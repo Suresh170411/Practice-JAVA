2d7639c7c7d311a02acdd5242a1e518aeccf7931
Suresh17 <suresh.20fk@gmail.com>
2022-09-23 18:12:22 +0530

    CODE: first hashmap program added

---
+++ hmPractice.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class hmPractice {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        hm.put("India", 50);
+        hm.put("China", 150);
+        hm.put("Pakistan", 40);
+
+        // insertion 
+        System.out.println(hm);
+
+        // lookup or search 
+        // System.out.println(hm.containsKey("India"));
+        if (hm.containsKey("China")){
+            System.out.println("Yes the key is present in the HashMap");
+        }else {
+            System.out.println("No the key is not present in the HashMap");
+        }
+    }
+}
