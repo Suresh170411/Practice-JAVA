29c96fdfaa26fdb1c509cf7a4d4c90ef196ea499
Suresh17 <suresh.20fk@gmail.com>
2022-09-23 18:21:39 +0530

    CODE: hashmap program practice GET function

--- hmPractice.java
+++ hmPractice.java
@@ -12,11 +12,18 @@ public class hmPractice {
         System.out.println(hm);
 
         // lookup or search operation
-        // System.out.println(hm.containsKey("India"));
+        // System.out.println(hm.containsKey("India"));  --> return TRUE OR FALSE;
+
         if (hm.containsKey("China")){
             System.out.println("Yes the key is present in the HashMap");
         }else {
             System.out.println("No the key is not present in the HashMap");
         }
+
+
+        // it returns the value which is assosiate with corresponding key;
+
+        System.out.println(hm.get("China")); // key exist
+        System.out.println(hm.get("Indonesia")); // key doesn't exitst
     }
 }
