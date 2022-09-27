2d521984f361ff72878e1df42d966309d40ee538
Suresh17 <suresh.20fk@gmail.com>
2022-09-27 07:48:14 +0530

    JAVA: StringBuilder practice

--- StrBuilder.java
+++ StrBuilder.java
@@ -9,5 +9,19 @@ public class StrBuilder {
         }
         System.out.println(new_sb);
 
+
+        // by not using another StringBuilder
+        for (int i=0; i<sb.length()/2; i++){
+            int start = i;
+            int last = sb.length() - 1 - i;
+                                                            
+            char front_char = sb.charAt(start);         // by using same stringbuilder str
+            char back_char = sb.charAt(last);
+
+            sb.setCharAt(start, back_char);
+            sb.setCharAt(last, front_char);
+        }
+        System.out.println(sb);
+
     }
 }
