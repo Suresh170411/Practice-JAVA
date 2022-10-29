15083e67c842fa97416bb7e34c28e72d5c02d877
suresh170411 <suresh.20fk@gmail.com>
2022-10-29 15:38:40 +0530

    PRACTICE : bubble sort

--- Basics/BubbleSort.java
+++ Basics/BubbleSort.java
@@ -1,25 +1,51 @@
+import java.util.Arrays;
+
 public class BubbleSort {
 
-    public static  void printArr(int [] arr ){
-        for (int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+    // public static  void printArr(int [] arr ){
+    //     for (int i=0; i<arr.length; i++){
+    //         System.out.print(arr[i]+" ");
+    //     }
+    //     System.out.println();
+    // }
+    // public static void main(String[] args) {
+        
+    //     int [] arr = {3,5,2,0,8,4,7};
+
+    //     for (int i=0; i<arr.length-1; i++){
+    //         for (int j=0; j<arr.length-i-1; j++){
+    //             if (arr[j] > arr[j+1]){
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j+1];
+    //                 arr[j+1] = temp;
+    //             }
+    //         }
+    //     }
+    //     printArr(arr);
+
+    // }
+
+    public static int[] swap(int [] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        return arr;
     }
     public static void main(String[] args) {
         
-        int [] arr = {3,5,2,0,8,4,7};
+        int [] arr = {4,2,6,3,7,1,9,0};
+        int n = arr.length;
 
-        for (int i=0; i<arr.length-1; i++){
-            for (int j=0; j<arr.length-i-1; j++){
+        for (int i=0; i<n-1; i++){
+            for (int j=0; j<n-i-1; j++){
                 if (arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    swap(arr, j, j+1);
                 }
             }
         }
-        printArr(arr);
+
+        System.out.println(Arrays.toString(arr));
 
     }
 }
