124de528ed1de2f9963e495c650cbd554d646155
suresh170411 <suresh.20fk@gmail.com>
2022-11-02 23:28:25 +0530

    RECURSION: calculating factorial using recursion

---
+++ Recursion/RecFacto.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class RecFacto {
+    public static int calcFacto(int n){
+
+        if (n == 1 || n==0){
+            return 1;
+        }
+
+        int fact_nm1 = calcFacto(n-1);      // calculation FACTORIAL using RECURSION
+        int fact_n = n*fact_nm1;
+        return fact_n;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        int ans = calcFacto(n);
+        System.out.println(ans);
+    }
+}
