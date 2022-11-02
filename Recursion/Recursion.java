23a294222b33bdd9e8eac90a1838e055d282dde2
suresh170411 <suresh.20fk@gmail.com>
2022-11-02 20:24:05 +0530

    RECURSION: practice reverse number printing

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,16 +1,22 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class Recursion {
+
     public static void printNum(int n){
-        if (n == 0){
-            return;                     // reversing a number using recursion
+        if(n == 0){
+            return;
         }
         System.out.print(n+" ");
         printNum(n-1);
+
     }
-    
     public static void main(String[] args) {
-        int n = 5;
-        printNum(n);
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        printNum(n);  // calling out the RECURSIVE function/method
     }
 }
