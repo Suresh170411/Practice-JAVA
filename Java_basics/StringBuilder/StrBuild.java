0503326a29c1abab873a09085460f689cd69d67f
suresh170411 <suresh.20fk@gmail.com>
2022-11-11 21:36:38 +0530

    PRACTICE: string builder REPLACE

--- Java_basics/StringBuilder/StrBuild.java
+++ Java_basics/StringBuilder/StrBuild.java
@@ -6,9 +6,15 @@ public class StrBuild {
         StringBuilder sb = new StringBuilder("masai");
         System.out.println(sb.reverse()); // reversing an string using string builder (iasam)
 
+
         StringBuilder message = new StringBuilder("Welcome");
         message.append(" to masai");
-
         System.out.println(message);        // Welcome to masai
+
+
+        StringBuilder rep = new StringBuilder("Hello");
+        rep.replace(0, 1, "K");             // index 0 and 1 exclude
+
+        System.out.println(rep);        // ---> Kello
     }
 }
