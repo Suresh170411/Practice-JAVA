959304971f43142647faca16c957829b97cfe0cc
suresh170411 <suresh.20fk@gmail.com>
2022-11-11 21:30:31 +0530

    PRACTICE: string builder reverse the string

---
+++ Java_basics/StringBuilder/StrBuild.java
@@ -0,0 +1,10 @@
+package Java_basics.StringBuilder;
+
+public class StrBuild {
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("masai");
+
+        System.out.println(sb.reverse()); // reversing an string using string builder
+    }
+}
