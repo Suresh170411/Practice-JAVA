ebfa22c4f600c958b98461efce1ac0667511c551
suresh170411 <suresh.20fk@gmail.com>
2022-11-11 21:33:55 +0530

    PRACTICE: string builder append

--- Java_basics/StringBuilder/StrBuild.java
+++ Java_basics/StringBuilder/StrBuild.java
@@ -4,7 +4,11 @@ public class StrBuild {
     public static void main(String[] args) {
         
         StringBuilder sb = new StringBuilder("masai");
+        System.out.println(sb.reverse()); // reversing an string using string builder (iasam)
 
-        System.out.println(sb.reverse()); // reversing an string using string builder
+        StringBuilder message = new StringBuilder("Welcome");
+        message.append(" to masai");
+
+        System.out.println(message);        // Welcome to masai
     }
 }
