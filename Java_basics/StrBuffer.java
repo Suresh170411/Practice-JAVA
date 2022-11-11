e26f34343d5ea3d3c793323190d860ca6235f97e
suresh170411 <suresh.20fk@gmail.com>
2022-11-11 21:25:40 +0530

    PRACTICE:StringBuffer class

---
+++ Java_basics/StrBuffer.java
@@ -0,0 +1,15 @@
+package Java_basics;
+
+public class StrBuffer {
+    public static void main(String[] args) {
+        
+        StringBuffer sb = new StringBuffer("Masai");
+        
+        System.out.println("STRING BUFFER: "+sb);       // --> Masai
+
+        String str = new String(sb);
+
+        System.out.println("STRING: "+str);         // --> Masai
+
+    }
+}
