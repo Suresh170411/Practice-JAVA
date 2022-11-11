358a2f73541d2f80226490cc4d7e083bd8122624
suresh170411 <suresh.20fk@gmail.com>
2022-11-11 21:17:56 +0530

    PRACTICE:STRING class char

--- Java_basics/StringPractice.java
+++ Java_basics/StringPractice.java
@@ -4,12 +4,18 @@ public class StringPractice {
 
     public static void check_byte_arr(byte [] arr){
         String str = new String(arr);
-
         System.out.println("The output is : "+str); //--> def
+    }
+
+    public static void check_char_arr(char [] arr){
+        String str = new String(arr);
+        System.out.println(str);    // --> Masai
     }
     public static void main(String[] args) {
         byte [] byte_arr = {100,101,102};
         check_byte_arr(byte_arr);
 
+        char [] char_arr = {'M','a','s','a','i'};
+        check_char_arr(char_arr);
     }
 }
