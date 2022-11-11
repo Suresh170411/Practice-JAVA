bef042cab36978c50aba3c502bd9a730a04b51b6
suresh170411 <suresh.20fk@gmail.com>
2022-11-11 21:49:47 +0530

    PRACTICE: string compareTo equals

--- Java_basics/String/EqualsMethod.java
+++ Java_basics/String/EqualsMethod.java
@@ -20,10 +20,10 @@ public class EqualsMethod {
         String str2 = "Sachin";
         String str3 = "Ratan";
 
-        System.out.println(str1.compareTo(str2)); // 0
-        System.out.println(str1.compareTo(str3)); // 1   -->   (str1 > str3)
+        System.out.println(str1.compareTo(str2)); //  0
+        System.out.println(str1.compareTo(str3)); //  1   -->  (str1 > str3)
         System.out.println(str3.compareTo(str1)); // -1  -->   (str3 < str1)
-        
+
     }
 
 }
