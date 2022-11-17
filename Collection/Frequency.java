de37751052c517aa2cf2d4191c9809f972b44812
suresh170411 <suresh.20fk@gmail.com>
2022-11-17 17:43:29 +0530

    ForEach loop for hashmap

--- Collection/Frequency.java
+++ Collection/Frequency.java
@@ -7,11 +7,18 @@ public class Frequency {
     public static void main(String[] args) {
         int [] arr = {2,4,1,5,1,5,1};
 
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        HashMap<Integer,String> hm = new HashMap<>();
 
-        for (int i=0; i<arr.length; i++){
-            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
-        }
+        // for (int i=0; i<arr.length; i++){
+        //     hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        // }
+
+        hm.put(1, "Apple");
+        hm.put(2, "Orange");
+        hm.put(3, "Banana");
+        hm.put(4, "Grape");
+
+        hm.forEach((k,v)->System.out.println(k+"=="+v));
     }
 
 }
