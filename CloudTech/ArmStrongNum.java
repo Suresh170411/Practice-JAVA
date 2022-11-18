e50db1d9565119bad144045fdc8ea2355f1a1c70
suresh170411 <suresh.20fk@gmail.com>
2022-11-19 01:05:53 +0530

    arm strong number

---
+++ CloudTech/ArmStrongNum.java
@@ -0,0 +1,11 @@
+package CloudTech;
+
+public class ArmStrongNum {     // check ARM STRONG num (1^3 + 5^3 + 3^3 = 1+125+27 = 153)
+    public static void main(String[] args) {
+        int input = 153;
+    }
+
+    public static boolean isArmStrong(int input){
+        
+    }
+}
