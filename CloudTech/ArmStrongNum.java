280183b96e200d24df8d19bc30155e97a3dd99f1
suresh170411 <suresh.20fk@gmail.com>
2022-11-19 01:15:30 +0530

    ArmStrong number check java

--- CloudTech/ArmStrongNum.java
+++ CloudTech/ArmStrongNum.java
@@ -2,26 +2,27 @@ package CloudTech;
 
 public class ArmStrongNum {     // check ARM STRONG num (1^3 + 5^3 + 3^3 = 1+125+27 = 153)
     public static void main(String[] args) {
-        int input = 153;
+        int input = 1634; //153
 
-        // if(isArmStrong(input)){
-        //     System.out.println("The number is armStrong");
-        // }else {
-        //     System.out.println("This number is not ArmStrong");
-        // }
-
-        System.out.println(countDigit(input));
+        if(isArmStrong(input)){
+            System.out.println("The number is armStrong");
+        }else {
+            System.out.println("This number is not ArmStrong");
+        }
     }
 
     public static boolean isArmStrong(int input){
         int j = input;
         int digit = 0;
         int sum = 0;
+
+        int numberOfDigit = countDigit(input);
+
         while(j > 0){
             digit = j%10;
             j = j/10;
 
-            sum+= digit*digit*digit;
+            sum+= Math.pow(digit, numberOfDigit);
         }
         return input == sum;
     }
