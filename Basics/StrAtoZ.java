bae94a9d18921b625a29de6fdbb506694e27bfd1
suresh170411 <suresh.20fk@gmail.com>
2022-11-18 23:52:50 +0530

    string a to z print practice

---
+++ Basics/StrAtoZ.java
@@ -0,0 +1,15 @@
+public class StrAtoZ {
+    public static void main(String[] args) {
+        char i;
+
+        for (i='A'; i<'Z'; i++){
+            System.out.print(i+" ");
+        }
+        
+        System.out.println();
+
+        for (i='a'; i<'z'; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
