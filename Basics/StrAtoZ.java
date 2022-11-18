0a1df4dcb711f0531343ed2c7689b966eb17251c
suresh170411 <suresh.20fk@gmail.com>
2022-11-19 00:00:27 +0530

    a to z print java practice

--- Basics/StrAtoZ.java
+++ Basics/StrAtoZ.java
@@ -2,10 +2,10 @@ public class StrAtoZ {
     public static void main(String[] args) {
         char i;
 
-        for (i='A'; i<'Z'; i++){
+        for (i='A'; i<'Z'; i++){        // it will print from a to z normally
             System.out.print(i+" ");
         }
-        
+
         System.out.println();
 
         for (i='a'; i<'z'; i++){
