f9e22653461d78a4741a29d99fcfa3020f2dd256
suresh170411 <suresh.20fk@gmail.com>
2022-11-19 21:18:27 +0530

    sorting string

--- Java_basics/String/StrSortBrute.java
+++ Java_basics/String/StrSortBrute.java
@@ -2,28 +2,37 @@ package Java_basics.String;
 
 public class StrSortBrute {
     public static void main(String[] args) {
-        String name = "Suresh";
-
-        sortStr(name);
-    }
-
-    public static void swap(char [] arr, int i, int j){
-        char temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    public static void sortStr(String str){
-
-        char [] res = str.toCharArray();
-
-        for (int i=0; i<res.length-1; i++){
-            for (int j=0; j<res.length-i-1; j++){
-                if (res[j] > res[j+1]){
-                    swap(res, j, j+1);
+        // Custom string input
+        String str = "geeksforgeeks";
+       
+        // Converting string into an array for computation
+        char arr[] = str.toCharArray();
+ 
+        // Nested loops for comparison of characters
+        // in above character array
+       
+        char temp;
+ 
+        int i = 0;
+        while (i < arr.length) {
+            int j = i + 1;
+            while (j < arr.length) {
+                if (arr[j] < arr[i]) {
+                   
+                    // Comparing the characters one by one
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
+                j += 1;
             }
+            i += 1;
         }
-        System.out.println(res); 
+ 
+        // By now loop is done means we have
+        // iterated the whole array
+        System.out.println(arr);
+    
     }
+
 }
