28d076558f0281a79df1fa5efcf08f5f17bfc5aa
suresh170411 <suresh.20fk@gmail.com>
2022-11-19 20:48:28 +0530

    sort string in java using SORT

--- Java_basics/String/StringSort.java
+++ Java_basics/String/StringSort.java
@@ -16,7 +16,7 @@ public class StringSort {
         return new String(str);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        // ALAN FREQUNCY
 
         String str = "Hello";
         int n = str.length();
