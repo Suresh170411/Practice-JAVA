c317086e8a2c86e8951c7fa5dc314503df962104
suresh170411 <suresh.20fk@gmail.com>
2022-11-19 21:17:17 +0530

    sort string

--- Java_basics/String/StrSortBrute.java
+++ Java_basics/String/StrSortBrute.java
@@ -2,13 +2,28 @@ package Java_basics.String;
 
 public class StrSortBrute {
     public static void main(String[] args) {
-        
+        String name = "Suresh";
+
+        sortStr(name);
     }
+
+    public static void swap(char [] arr, int i, int j){
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public static void sortStr(String str){
 
         char [] res = str.toCharArray();
 
-        
-
+        for (int i=0; i<res.length-1; i++){
+            for (int j=0; j<res.length-i-1; j++){
+                if (res[j] > res[j+1]){
+                    swap(res, j, j+1);
+                }
+            }
+        }
+        System.out.println(res); 
     }
 }
