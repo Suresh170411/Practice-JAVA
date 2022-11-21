5c8e71bb556db07913dc5e6df0d47f2123c8d2a3
suresh170411 <suresh.20fk@gmail.com>
2022-11-21 11:02:39 +0530

    check uppercase and lowercase

--- Java_basics/String/AtoZ.java
+++ Java_basics/String/AtoZ.java
@@ -15,6 +15,19 @@ public class AtoZ {
         for (char i='a'; i<'z'; i++){
             System.out.print(i+" ");
         }
+        System.out.println();
+        for (char i='A'; i<'Z'; i++){
+            System.out.print(i+" ");
+        }
+
+        System.out.println();
+        System.out.println("=============================");
+
+        if(str.matches("[a-zA-Z]+")){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
 
     }
 }
