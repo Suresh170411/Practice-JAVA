67e724fdd95327cf942ee316dcb6573e06e9a1cd
suresh170411 <suresh.20fk@gmail.com>
2022-11-21 10:54:33 +0530

    String for loop from a to z

--- Java_basics/String/AtoZ.java
+++ Java_basics/String/AtoZ.java
@@ -1,8 +1,20 @@
 package Java_basics.String;
 
+import java.util.Arrays;
+
 public class AtoZ {
     public static void main(String[] args) {
         
-        
+
+        String str = "Hello";
+
+        char [] ch = str.toCharArray();
+
+        System.out.println(Arrays.toString(ch));
+
+        for (char i='a'; i<'z'; i++){
+            System.out.print(i+" ");
+        }
+
     }
 }
