631724658a7df4a7303deecdcdb87ab49c9c2e05
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 21:22:01 +0530

    inplace reverse rotate array

--- DSA/RotateArr.java
+++ DSA/RotateArr.java
@@ -1,28 +1,25 @@
 package DSA;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class RotateArr {
     public static void main(String[] args) {
         int [] arr = {1,2,3,4};
 
         int k = 2;
-
+        rotate(arr, k);
         
     }
-    public void rotate(int[] nums, int k) {
+    public static void rotate(int[] arr, int k) {
         // step1: reverse the array
         // step2: split the array into two.
         // step3: reverse both the two parts.
         
         //taking k modulo for edge cases
-        k = k%nums.length;
-        swap(nums, 0, nums.length-1);
-        swap(nums, 0, k-1);
-        swap(nums, k, nums.length-1);
+        k = k%arr.length;
+        swap(arr, 0, arr.length-1);
+        swap(arr, 0, k-1);
+        swap(arr, k, arr.length-1);
     }
-    public void swap(int[] arr, int start, int end){
+    public static void swap(int[] arr, int start, int end){
         while(start<end){
             int temp = arr[start];
             arr[start] = arr[end];
