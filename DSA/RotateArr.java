a2f10723411fccec5c73842e2178634a4ccddf9d
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 19:52:20 +0530

    rotate array with n TC

--- DSA/RotateArr.java
+++ DSA/RotateArr.java
@@ -1,5 +1,26 @@
 package DSA;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class RotateArr {
-    
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4};
+
+        int k = 2;
+
+        int [] res = new int [arr.length];
+
+        for (int i=k; i<arr.length; i++){
+            res[i] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(res));
+
+        for (int i=0; i<k; i++){
+            res[k+i] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(res));
+    }
 }
