fb71c9f0e05f0f4c8b22b9bb2462d3c4d394ba26
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 21:24:28 +0530

    inplace reverse rotate array

--- DSA/RotateArr.java
+++ DSA/RotateArr.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class RotateArr {
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4};
+        int [] arr = {1,2,3,4,5,6,7};
 
-        int k = 2;
+        int k = 3;
         rotate(arr, k);
 
         System.out.println(Arrays.toString(arr));
