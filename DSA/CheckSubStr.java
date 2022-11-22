afde666fe61b35797b7f94a5b61420c5c41ab0e6
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 19:29:54 +0530

    subStr in java with n^2

--- DSA/CheckSubStr.java
+++ DSA/CheckSubStr.java
@@ -3,5 +3,12 @@ package DSA;
 public class CheckSubStr {
     public static void main(String[] args) {
         
+        String str = "aman";
+        
+        for (int i=0; i<str.length(); i++){
+            for (int j=i; j<str.length(); j++){
+                
+            }
+        }
     }
 }
