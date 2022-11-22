d031322643bba0f069451321a52e0a0794287cb6
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 09:26:31 +0530

    first half lower second half uppercase

---
+++ CloudTech/HalfUpperCase.java
@@ -0,0 +1,11 @@
+package CloudTech;
+
+public class HalfUpperCase {
+    public static void main(String[] args) {
+        String str = "Hello there Hello there";
+
+        int mid = str.length()/2;
+
+        
+    }
+}
