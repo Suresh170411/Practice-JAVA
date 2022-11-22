78da94a7a0961a13b61349ebf5274e2648cf420d
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 12:55:49 +0530

    Char practice with toCharArray in java

---
+++ Java_basics/String/CharStr.java
@@ -0,0 +1,13 @@
+package Java_basics.String;
+
+import java.util.Arrays;
+
+public class CharStr {
+    public static void main(String[] args) {
+        String str = "Hello";
+
+        char [] arr = str.toCharArray();
+
+        System.out.println(arr); // Hello
+    }
+}
