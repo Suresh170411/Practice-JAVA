04b1d6492f9ba75e2f9c5b5fe7e47b63e05dc2b2
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 19:23:07 +0530

    check if it's a triangle java

--- DSA/TriangleCheck.java
+++ DSA/TriangleCheck.java
@@ -2,4 +2,16 @@ package DSA;
 
 public class TriangleCheck {
     
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 8;
+        int c = 9;
+
+        if( (a+b) > c && (a+c) > b && (c+b) > a){
+            System.out.println("Yes This is a Triangle");
+        }else {
+            System.out.println("Not a triangle");
+        }
+    }
+
 }
