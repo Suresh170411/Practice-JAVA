1ec243fd63940716176a78e2809364261535c3e6
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 12:46:56 +0530

    string sort practice

--- CloudTech/SortString.java
+++ CloudTech/SortString.java
@@ -1,5 +1,35 @@
 package CloudTech;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SortString {
     
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A String you want to sort");
+
+        String str = sc.next();
+
+        // String str = "aman";
+
+        String res = sortString(str);
+
+        System.out.println("Sorted String is--->"+res);
+
+    }
+
+    public static String sortString(String inputString)
+    {
+        // Converting input string to character array
+        char tempArray[] = inputString.toCharArray();
+ 
+        // Sorting temp array using
+        Arrays.sort(tempArray);
+ 
+        // Returning new sorted string
+        return new String(tempArray);
+    }
+
 }
