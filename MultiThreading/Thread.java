72978016f8fabb6ed57e03b78e876907c6660302
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 19:19:51 +0530

    multithreading

---
+++ MultiThreading/Thread.java
@@ -0,0 +1,16 @@
+package MultiThreading;
+
+public class Thread implements Runnable {
+    
+    @Override
+    public void run(){
+        System.out.println("Hello There. How are you doing..?");
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread();
+        
+        t1.run();
+        System.out.println("NO way man..");
+    }
+}
