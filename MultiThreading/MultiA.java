db4cfa8fbca380c7db38322c07cedff306d5c152
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 14:55:19 +0530

    practice multiThreading

---
+++ MultiThreading/MultiA.java
@@ -0,0 +1,22 @@
+package MultiThreading;
+
+public class MultiA extends Thread {
+    @Override
+    public void run(){
+        for (int i=0; i<5; i++){
+            System.out.println("Inside run mehtod");
+        }
+    }
+    public static void main(String[] args) {
+        MultiA m = new MultiA();
+
+        // m.run();
+        m.start();
+
+        for (int i=0; i<5; i++){
+            System.out.println("Inside main method..");
+        }
+
+        System.out.println("end of main");
+    }
+}
