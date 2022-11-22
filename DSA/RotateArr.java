c5e59ab7a92e2c4b9349c31f39ea40e9e2d7b646
suresh170411 <suresh.20fk@gmail.com>
2022-11-22 21:08:38 +0530

    totate arr in o^n

--- DSA/RotateArr.java
+++ DSA/RotateArr.java
@@ -9,18 +9,26 @@ public class RotateArr {
 
         int k = 2;
 
-        int [] res = new int [arr.length];
-
-        for (int i=k; i<arr.length; i++){
-            res[i] = arr[i];
+        
+    }
+    public void rotate(int[] nums, int k) {
+        // step1: reverse the array
+        // step2: split the array into two.
+        // step3: reverse both the two parts.
+        
+        //taking k modulo for edge cases
+        k = k%nums.length;
+        swap(nums, 0, nums.length-1);
+        swap(nums, 0, k-1);
+        swap(nums, k, nums.length-1);
+    }
+    public void swap(int[] arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-
-        System.out.println(Arrays.toString(res));
-
-        for (int i=0; i<k; i++){
-            res[k+i] = arr[i];
-        }
-
-        System.out.println(Arrays.toString(res));
     }
 }
