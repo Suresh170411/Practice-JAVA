38fc0ba8bc62db6a7190e869a7e98788baf968b8
suresh170411 <suresh.20fk@gmail.com>
2022-11-23 17:49:05 +0530

    make wave of an array

--- DSA/WavePattern.java
+++ DSA/WavePattern.java
@@ -1,5 +1,31 @@
 package DSA;
 
+import java.util.Arrays;
+
 public class WavePattern {
+    public static void main(String[] args) {
+        int [] arr = {9, 8, 4, 10, 3, 6, 5, 7, 1, 2};
+
+        makeWaveOfArray(10, arr);
+    }
     
+    public static void makeWaveOfArray(int n, int [] arr){
+        
+        Arrays.sort(arr);
+        
+        for (int i=0; i<n-1; i = i+2){
+            swap(arr,i,i+1);
+        }
+        
+        for (int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+    
+    public static void swap(int [] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
