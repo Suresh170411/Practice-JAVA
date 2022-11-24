67f304d481d5bf8148843cb05ecd7a89e9a60049
suresh170411 <suresh.20fk@gmail.com>
2022-11-24 22:28:54 +0530

    check isUpperCase isDigit and isLowerCase

--- Java_basics/String/StrUpper.java
+++ Java_basics/String/StrUpper.java
@@ -8,12 +8,35 @@ public class StrUpper {
 
         boolean flag = false;
         for (int i=0; i<n; i++){
-            if (Character.isUpperCase(str.charAt(i)) || 
-            Character.isLowerCase(str.charAt(i)) || 
+            if (
             Character.isDigit(str.charAt(i))){
                 flag = true;
             }
         }
         System.out.println(flag);
+    }
+
+    public static boolean upperCaseCheck(String str){
+        
+        boolean flag = false;
+        
+        for (int i=0; i<str.length(); i++){
+            if (Character.isUpperCase(str.charAt(i))){
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    public static boolean loweCaseCheck(String str){
+
+        boolean flag = false;
+
+        for (int i=0; i<str.length(); i++){
+            if (Character.isLowerCase(str.charAt(i))){
+                flag = true;
+            }
+        }
+        return flag;
     }
 }
