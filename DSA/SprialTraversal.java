1c46d2ed6e508e0b904545422e40de31b0933418
suresh170411 <suresh.20fk@gmail.com>
2022-11-24 22:43:29 +0530

    Spiral Traversal

--- DSA/SprialTraversal.java
+++ DSA/SprialTraversal.java
@@ -4,6 +4,14 @@ public class SprialTraversal {
     public static void main(String[] args) {
         int [][] mat = {{1,2,3,4},{5,6,7,8},{1,2,3,4},{5,6,7,8}};
 
+        int N = mat.length;
+        int M = mat[0].length;
+
+        int top = 0;
+        int bottom = N-1;
+        int left = 0;
+        int right = M-1;
+
         
 
     }
