06b9868b937cf049e42de18f9e61b46a6398fa7e
suresh170411 <suresh.20fk@gmail.com>
2022-11-24 18:21:46 +0530

    arraylist without object type

--- Collection/ArrayList/ArrListNEW.java
+++ Collection/ArrayList/ArrListNEW.java
@@ -1,7 +1,18 @@
 package Collection.ArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrListNEW {
     public static void main(String[] args) {
-        
+        List list = new ArrayList();
+
+        list.add("Hello");
+        list.add(10);
+
+        System.out.println(list);
+
+        System.out.println(list.get(0) instanceof Object);
+        System.out.println(list.get(1) instanceof Integer);
     }
 }
