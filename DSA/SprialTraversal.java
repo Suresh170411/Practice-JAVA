07189b6521141eae00ba3e0bc95c0c51d5b52b0d
suresh170411 <suresh.20fk@gmail.com>
2022-11-24 23:01:07 +0530

    Spiral Traversal

--- DSA/SprialTraversal.java
+++ DSA/SprialTraversal.java
@@ -2,7 +2,7 @@ package DSA;
 
 public class SprialTraversal {
     public static void main(String[] args) {
-        int [][] mat = {{1,2,3,4},{5,6,7,8},{1,2,3,4},{5,6,7,8}};
+        int [][] mat = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 
         int N = mat.length;
         int M = mat[0].length;
@@ -13,11 +13,34 @@ public class SprialTraversal {
         int right = M-1;
         int dir = 1;
 
-        while (left < right && top < bottom){
-            for (int i=top; i<=bottom; i++){
-                Syso
+
+        while (left <= right && top <= bottom){
+            
+            if(dir == 1){
+                for (int i=top; i<=bottom; i++){
+                    System.out.print(mat[i][left]+" ");
+                }
+                left++;
+                dir = 2;
+            }else if(dir == 2){
+                for (int i=left; i<=right; i++){
+                    System.out.print(mat[bottom][i]+" ");
+                }
+                bottom--;
+                dir = 3;
+            }else if(dir == 3){
+                for (int i=bottom; i>=top; i--){
+                    System.out.print(mat[i][right]+" ");
+                }
+                right--;
+                dir = 4;
+            }else {
+                for (int i=right; i>=left; i--){
+                    System.out.print(mat[top][i]+" ");
+                }
+                top++;
+                dir = 1;
             }
         }
-
     }
 }
