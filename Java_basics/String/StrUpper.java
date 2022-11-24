ff9e9283b7ce0b4fe0578f87e985d0db8f9997bd
suresh170411 <suresh.20fk@gmail.com>
2022-11-24 22:22:20 +0530

    check isUpperCase in string

--- Java_basics/String/StrUpper.java
+++ Java_basics/String/StrUpper.java
@@ -4,6 +4,14 @@ public class StrUpper {
     public static void main(String[] args) {
         String str = "aBcT";
 
-        
+        int n = str.length();
+
+        boolean flag = false;
+        for (int i=0; i<n; i++){
+            if (Character.isUpperCase(str.charAt(i))){
+                flag = true;
+            }
+        }
+        System.out.println(flag);
     }
 }
