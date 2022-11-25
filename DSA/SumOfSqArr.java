54f05babd1a309d4f1459b92d89d99c9034d5b83
suresh170411 <suresh.20fk@gmail.com>
2022-11-25 20:41:32 +0530

    sum of Square number

--- DSA/SumOfSqArr.java
+++ DSA/SumOfSqArr.java
@@ -1,32 +1,20 @@
 package DSA;
-
 public class SumOfSqArr {
     public static void main(String[] args) {
         int n = 0;
 
-        boolean flag = check(n);
-
-        if (flag){
-            System.out.println("YES");
-        }else {
-            System.out.println("NO");
-        }
+        System.out.println(check(n));
     }
 
     public static boolean check(int n){
         
-        boolean flag = false;
-        
-        if(n == 0){
-            flag = true;
-        }
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                if(i*i + j*j == n){
-                    flag = true;
+        for (long i=0; i*i <= n; i++){
+            for (long j=0; j*j <= n; j++){
+                if (i*i + j*j == n){
+                    return true;
                 }
             }
         }
-        return flag;
+        return false;
     }
 }
--- DSA/SumOfsqArray.java
+++ DSA/SumOfsqArray.java
@@ -1,21 +1,5 @@
 package DSA;
 
 public class SumOfsqArray {
-    public static void main(String[] args) {
-        int n = 0;
-
-        System.out.println(check(n));
-    }
-
-    public static boolean check(int n){
-        
-        for (long i=0; i*i <= n; i++){
-            for (long j=0; j*j <= n; j++){
-                if (i*i + j*j == n){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+    
 }
