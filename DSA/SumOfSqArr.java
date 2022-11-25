6f7c79e5c1110bd930c1f79a7ec979806566ccdd
suresh170411 <suresh.20fk@gmail.com>
2022-11-25 20:30:05 +0530

    Sum of Square Numbers

--- DSA/SumOfSqArr.java
+++ DSA/SumOfSqArr.java
@@ -2,6 +2,25 @@ package DSA;
 
 public class SumOfSqArr {
     public static void main(String[] args) {
+        int n = 5;
+
+        boolean flag = check(n);
+
+        if (flag){
+            System.out.println("");
+        }
+    }
+
+    public static boolean check(int n){
         
+        boolean flag = false;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if(i*i+j*j == n){
+                    flag = true;
+                }
+            }
+        }
+        return flag;
     }
 }
