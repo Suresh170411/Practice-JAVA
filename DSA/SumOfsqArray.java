6a437c8af81dc566184b2d13ce143e8f6e4100a3
suresh170411 <suresh.20fk@gmail.com>
2022-11-25 20:43:43 +0530

    check square of number

--- DSA/SumOfsqArray.java
+++ DSA/SumOfsqArray.java
@@ -1,5 +1,26 @@
 package DSA;
-
 public class SumOfsqArray {
-    
+    public static void main(String[] args) {
+        int n = 0;
+
+        System.out.println(check(n));
+    }
+
+    public static boolean check(int n){
+        long i=0,j=(long)Math.sqrt(n);
+        
+        while(i<=j){
+            long curr = i*i + j*j;
+            if(curr < n){
+                i++;
+            } 
+            else if(curr > n){
+                j--;
+            } 
+            else{
+                return true;
+            } 
+        }
+        return false;
+    }
 }
