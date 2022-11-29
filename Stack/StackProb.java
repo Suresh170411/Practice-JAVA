f9851e6dd7381abc4e542c432e86cbdbe95dc0d6
suresh170411 <suresh.20fk@gmail.com>
2022-11-29 20:58:45 +0530

    stack problem

---
+++ Stack/StackProb.java
@@ -0,0 +1,14 @@
+package Stack;
+
+import java.util.Stack;
+
+public class StackProb {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+
+        System.out.println(stack);
+    }
+}
