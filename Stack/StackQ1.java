bb5780c6c910b92d9ee37b378f8deedfa955ca6e
suresh170411 <suresh.20fk@gmail.com>
2022-11-30 19:55:14 +0530

    stack problem short string

---
+++ Stack/StackQ1.java
@@ -0,0 +1,13 @@
+package Stack;
+
+import java.util.Stack;
+
+public class StackQ1 {
+    public static void main(String[] args) {
+        
+        String str = "aaabccddd";
+
+        System.out.println(str.length());
+
+    }
+}
