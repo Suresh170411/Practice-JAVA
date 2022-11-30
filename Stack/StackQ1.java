fc62bf1c2200f9c98c11be67a035a8514e1dd80b
suresh170411 <suresh.20fk@gmail.com>
2022-11-30 20:10:36 +0530

    short string stack problem

--- Stack/StackQ1.java
+++ Stack/StackQ1.java
@@ -7,9 +7,33 @@ public class StackQ1 {
         
         String str = "aaabccddd";
 
+        char [] arr = str.toCharArray();
+
         int n = str.length();
 
-        
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : arr){
+            if (stack.isEmpty()) {
+                stack.push(c);
+            }else if(stack.peek() == c){
+                stack.pop();
+            }else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Character c : stack){
+            sb.append(c);
+        }
+
+        if (sb.length() == 0){
+            System.out.println("Empty String");
+        }else{
+            System.out.println(sb);
+        }
 
     }
 }
