b507ee3b563a008cdb74fe0151eeb05059d407eb
suresh170411 <suresh.20fk@gmail.com>
2022-12-03 10:50:56 +0530

    recursion factorial count

--- Recursion/RecFacto.java
+++ Recursion/RecFacto.java
@@ -2,10 +2,12 @@ package Recursion;
 
 public class RecFacto {
     public static int calcFacto(int n){
-        return 0;
+        if ( n == 0) return 1;
+        return n*calcFacto(n-1);
     }   
 
     public static void main(String[] args) {
-        
+        int n = 5;
+        System.out.println(calcFacto(n));
     }
 }
