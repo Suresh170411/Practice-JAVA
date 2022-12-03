1bc013fc6842abbd0e8219d698dd2194e8534a03
suresh170411 <suresh.20fk@gmail.com>
2022-12-03 10:53:09 +0530

    factorial print using recursion

--- Recursion/RecFacto.java
+++ Recursion/RecFacto.java
@@ -2,12 +2,22 @@ package Recursion;
 
 public class RecFacto {
     public static int calcFacto(int n){
-        if ( n == 0) return 1;
+
+        if (n == 1 || n==0){
+            return 1;
+        }
+
+        // int fact_nm1 = calcFacto(n-1);      // calculation FACTORIAL using RECURSION
+        // int fact_n = n*fact_nm1;
+        // return fact_n;
+
         return n*calcFacto(n-1);
-    }   
+    }
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(calcFacto(n));
+
+        int ans = calcFacto(n);
+        System.out.println("Factorial of "+n+" is : "+ans);
     }
 }
