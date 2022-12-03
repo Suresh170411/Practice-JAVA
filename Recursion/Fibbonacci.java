580cf81826c5d3c6b3af9c3949cebe2b1b12369f
suresh170411 <suresh.20fk@gmail.com>
2022-12-03 11:02:01 +0530

    fibbonacci problem recursion

--- Recursion/Fibbonacci.java
+++ Recursion/Fibbonacci.java
@@ -4,4 +4,13 @@ public class Fibbonacci {
     public static void main(String[] args) {
         
     }
+
+    public int fib(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        // if (n <= 1) return 1;
+
+        return fib(n-1)+fib(n-2);
+    }
 }
