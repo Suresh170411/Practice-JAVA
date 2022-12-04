f67e4ab9ae9ce30df1e44ef87cd5f5cc90625cd6
suresh170411 <suresh.20fk@gmail.com>
2022-12-04 23:36:16 +0530

    print reverse number using recursion java

--- Recursion/PrintNumsRev.java
+++ Recursion/PrintNumsRev.java
@@ -1,5 +1,13 @@
 package Recursion;
 
 public class PrintNumsRev {
-    
+    public static void main(String[] args) {
+        int n = 5;
+        printNums(n);
+    }
+    public static void printNums(int n){
+        if (n == 0) return;
+        System.out.println(n);
+        printNums(n-1);
+    }
 }
