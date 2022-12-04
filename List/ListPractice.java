9337f9d1b018d3a38da7efa9ab1e06c37601b4de
suresh170411 <suresh.20fk@gmail.com>
2022-12-04 20:56:06 +0530

    Listof method practice in list

--- List/ListPractice.java
+++ List/ListPractice.java
@@ -1,5 +1,15 @@
 package List;
 
+import java.util.List;
+
 public class ListPractice {
-    
+    public static void main(String[] args) {
+        
+
+
+    }
+
+    public static List<Integer> getList(){
+        return List.of(1,2,3,4,5,6);
+    }
 }
