5d24992209bbc263075283c55407daef94704a47
suresh170411 <suresh.20fk@gmail.com>
2022-12-05 21:57:27 +0530

    boolean sort

--- DSA/SortBoolean.java
+++ DSA/SortBoolean.java
@@ -5,8 +5,24 @@ import java.util.List;
 
 public class SortBoolean {
     public static void main(String[] args) {
-        boolean [] arr = {false,true,false,true};
-
+        boolean [] arr = {false,true,false,true,true};
+        
+        boolean[] arr1 = new boolean[arr.length];
+        int j =0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == true){
+                arr1[j] = true;
+                j++;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(i<j){
+                arr[i]=true;
+            }else{
+                arr[i]=false;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
         
     }
 }
