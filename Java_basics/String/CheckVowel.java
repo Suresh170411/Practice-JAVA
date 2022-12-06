4289ba532b48faa8da05d2e42ada11ab39e372ce
suresh170411 <suresh.20fk@gmail.com>
2022-12-06 19:28:30 +0530

    check vowel using pattern matchers

--- Java_basics/String/CheckVowel.java
+++ Java_basics/String/CheckVowel.java
@@ -6,14 +6,24 @@ import java.util.regex.Pattern;
 public class CheckVowel {
     public static void main(String[] args) {
 
-        String str = "aman";
+        String str = "ankushIsnotgOOD";
 
-        Pattern p = Pattern.compile("[aeiou]");
+        Pattern p = Pattern.compile("[aeiouAEIOU]");
         Matcher m = p.matcher(str);
-        int count = 0;
+
+        int vowel = 0;
         while(m.find()){
-            count++;
+            vowel++;
         }
-        System.out.println("Number of Vowel : "+count);
+        System.out.println("Number of vowel : "+vowel);
+
+        Pattern p1 = Pattern.compile("[^aeiouAEIOU]");
+        Matcher m1 = p1.matcher(str);
+
+        int consonent = 0;
+        while(m1.find()){
+            consonent++;
+        }
+        System.out.println("Number of Consonent : "+consonent);
     }
 }
