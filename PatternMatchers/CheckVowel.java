019fb4eb37987776d33c1feed4feaf51d8efbc7b
suresh170411 <suresh.20fk@gmail.com>
2022-12-06 19:30:04 +0530

    Pattern matcher check vowel

---
+++ PatternMatchers/CheckVowel.java
@@ -0,0 +1,29 @@
+package Java_basics.String;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CheckVowel {
+    public static void main(String[] args) {
+
+        String str = "ankushIsnotgOOD";
+
+        Pattern p = Pattern.compile("[aeiouAEIOU]");
+        Matcher m = p.matcher(str);
+
+        int vowel = 0;
+        while(m.find()){
+            vowel++;
+        }
+        System.out.println("Number of vowel : "+vowel);
+
+        Pattern p1 = Pattern.compile("[^aeiouAEIOU]");
+        Matcher m1 = p1.matcher(str);
+
+        int consonent = 0;
+        while(m1.find()){
+            consonent++;
+        }
+        System.out.println("Number of Consonent : "+consonent);
+    }
+}
