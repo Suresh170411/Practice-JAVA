425a830a697c0719415a85de2266ad3710ed681c
suresh170411 <suresh.20fk@gmail.com>
2022-12-11 10:17:03 +0530

    stack left largest elemnt

--- Stack/SmallestNearest.java
+++ Stack/SmallestNearest.java
@@ -1,11 +1,22 @@
 package Stack;
 
+import java.util.Stack;
+
 public class SmallestNearest {
     public static void main(String[] args) {
         
         int [] arr = {5,4,1,2,4,3};
+        int ans [] = new int [arr.length];
+        Stack<Integer> st = new Stack<>();
 
-        
+
+        for (int i=0; i<arr.length; i++){
+            while (!st.isEmpty() && st.peek() >= arr[i]){
+                st.pop();
+
+                if (st.isEmpty()) ans[i] = -1;
+            }
+        }
 
     }
 }
