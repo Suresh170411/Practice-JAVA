cd978f024d80315ea25f25c0f3821be931c44571
suresh170411 <suresh.20fk@gmail.com>
2022-12-11 10:38:45 +0530

    left smaller element stack

--- Stack/SmallestNearest.java
+++ Stack/SmallestNearest.java
@@ -33,9 +33,7 @@ public class SmallestNearest {
         int n = arr.length;
         int res = 0;
 
-        System.out.print(-1+" ");
-
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = i - 1; j >= 0; j--) {
                 if (arr[j] < arr[i]) {
                     System.out.print(arr[j] + " ");
