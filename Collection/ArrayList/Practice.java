804f7dab3aed55e67618bc29114ddc82e6773cb0
suresh170411 <suresh.20fk@gmail.com>
2022-12-15 00:09:16 +0530

    collection frame work arraylist

--- Collection/ArrayList/Practice.java
+++ Collection/ArrayList/Practice.java
@@ -1,9 +1,20 @@
 package Collection.ArrayList;
 
+import java.util.*;
 public class Practice {
     public static void main(String[] args) {
+
         List<Integer> list = new ArrayList<>();
         list.add(20);
+        list.add(30);
+        list.add(40);
+
         System.out.println(list);
+
+        List<Integer> newList = new ArrayList<>(4);
+        newList.add(10);
+        newList.addAll(1,list);
+        System.out.println(newList);
+
     }
 }
