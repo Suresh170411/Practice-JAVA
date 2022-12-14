1e7b69395a8e0c55c35663713f9edaac40797602
suresh170411 <suresh.20fk@gmail.com>
2022-12-14 23:27:03 +0530

    sub string problem

--- DSA/CheckSubStr.java
+++ DSA/CheckSubStr.java
@@ -3,7 +3,7 @@ package DSA;
 public class CheckSubStr {
     public static void main(String[] args) {
         
-        String str = "aman";
+        String str = "aman";    // print all substring of the given string
         
         for (int i=0; i<str.length(); i++){
             String bag = "";
