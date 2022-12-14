013e77d9f9630f9e7a6d0eb2149a87e36945e36d
suresh170411 <suresh.20fk@gmail.com>
2022-12-14 23:32:53 +0530

    Collection list .clear() check

---
+++ Collection/List/ClearCheck.java
@@ -0,0 +1,13 @@
+package Collection.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClearCheck {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(20,10,30,40,80);
+
+        System.out.println(list);
+    }
+}
