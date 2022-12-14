448587520a70afc59c5217b628afadd480555726
suresh170411 <suresh.20fk@gmail.com>
2022-12-15 00:01:02 +0530

    added list practice

--- Collection/ArrayList/Practice.java
+++ Collection/ArrayList/Practice.java
@@ -2,6 +2,8 @@ package Collection.ArrayList;
 
 public class Practice {
     public static void main(String[] args) {
-        
+        List<Integer> list = new ArrayList<>();
+        list.add(20);
+        System.out.println(list);
     }
 }
