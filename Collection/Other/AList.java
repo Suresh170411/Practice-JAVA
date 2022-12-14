4cd7456d9f2dfd12128ba1be573f064789202338
suresh170411 <suresh.20fk@gmail.com>
2022-12-14 23:37:49 +0530

    collection framework practice

--- Collection/Other/AList.java
+++ Collection/Other/AList.java
@@ -1,4 +1,4 @@
-package Collection;
+package Collection.Other;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- Collection/Other/Frequency.java
+++ Collection/Other/Frequency.java
@@ -1,4 +1,4 @@
-package Collection;
+package Collection.Other;
 
 import java.util.HashMap;
 
