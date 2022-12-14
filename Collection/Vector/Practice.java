b20a20ebfff99e4b458d4c8c502709c08950047a
suresh170411 <suresh.20fk@gmail.com>
2022-12-15 00:19:38 +0530

    vectror practice

--- Collection/Vector/Practice.java
+++ Collection/Vector/Practice.java
@@ -1,7 +1,13 @@
 package Collection.Vector;
 
+import java.util.Vector;
+
 public class Practice {
     public static void main(String[] args) {
-        
+        Vector<Integer> v = new Vector<>();
+        v.add(10);
+        v.add(20);
+
+        System.out.println(v);
     }
 }
