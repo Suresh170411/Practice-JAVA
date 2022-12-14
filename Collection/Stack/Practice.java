64d376dfbed6b3727d3132500c2e7f76ffd35d74
suresh170411 <suresh.20fk@gmail.com>
2022-12-15 00:26:44 +0530

    stack practice

--- Collection/Stack/Practice.java
+++ Collection/Stack/Practice.java
@@ -1,8 +1,26 @@
 package Collection.Stack;
 
+import java.util.Stack;
+
 public class Practice {
     public static void main(String[] args) {
         
-        
+        Stack<Integer> s = new Stack<>();
+        s.add(10);
+        s.add(20);
+        s.add(30);
+        System.out.println(s);
+
+        Stack<Integer> stack = new Stack<>();
+        stack.add(11);
+        stack.add(22);
+        stack.add(33);
+        stack.add(44);
+
+        System.out.println(stack);
+
+        for (Integer i : stack){
+            System.out.println(i);
+        }
     }
 }
