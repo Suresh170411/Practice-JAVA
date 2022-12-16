0f751d7485a84fd213d882ce52cf9565d300a09f
suresh170411 <suresh.20fk@gmail.com>
2022-12-16 08:28:43 +0530

    practice hashmap

--- HashMap/Practice.java
+++ HashMap/Practice.java
@@ -9,8 +9,8 @@ public class Practice {
         Map<Integer,Integer> hm = new HashMap<>();
         
         for (int i=0; i<arr.length; i++){
-            hm.put(arr[i],hm.getOrDefault(arr[i], 0)+1);
+            int x = arr[i];
+            if (x )
         }
-        System.out.println(hm);
     }
 }
