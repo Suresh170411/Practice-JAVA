dbc6c55f173d36bba669e49f953207b50107b2a2
suresh170411 <suresh.20fk@gmail.com>
2022-12-16 08:24:57 +0530

    hashmap practice

--- HashMap/Practice.java
+++ HashMap/Practice.java
@@ -9,7 +9,8 @@ public class Practice {
         int arr [] = {12,2,4,5,5,2,6};
 
         for (int i=0; i<arr.length; i++){
-            
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
         }
+        System.out.println(hm);
     }
 }
