3c5939e2cb64ab1d64bedbc6e442675f88cd012c
suresh170411 <suresh.20fk@gmail.com>
2022-12-16 08:26:21 +0530

    practice hashmap

--- HashMap/Practice.java
+++ HashMap/Practice.java
@@ -4,10 +4,9 @@ import java.util.HashMap;
 
 public class Practice {
     public static void main(String[] args) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
 
         int arr [] = {12,2,4,5,5,2,6};
-
+        HashMap<Integer,Integer> hm = new HashMap<>();
         
     }
 }
