9b2a5a22f08f3016856a051c9cb4cf16984feb0d
suresh170411 <suresh.20fk@gmail.com>
2022-12-16 08:48:05 +0530

    hm practice

--- HashMap/Practice.java
+++ HashMap/Practice.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Practice {
     public static void main(String[] args) {
 
-        int arr [] = {12,2,4,5,5,2,6};
+        int arr [] = {12,2,4,5,5,2,6};  //-------> hashmap practice
         Map<Integer,Integer> hm = new HashMap<>();
         
         for (int i=0; i<arr.length; i++){
