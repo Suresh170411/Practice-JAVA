01d875f69ed99b228f1846a0bcbedaf3bab064f1
suresh170411 <suresh.20fk@gmail.com>
2022-12-19 22:26:29 +0530

    print fibonacci recursion

--- NEW/Recursion/Practice6.java
+++ NEW/Recursion/Practice6.java
@@ -2,14 +2,14 @@ package NEW.Recursion;
 
 public class Practice6 {
     public static void main(String[] args) {
-        int a = 0, b = 1;
-        int n = 5;
+        int a = 0,b = 1, n = 7;
         System.out.println(a);
         System.out.println(b);
         fibo(a, b, n-2);
     }
     public static void fibo(int a, int b, int n){
         if (n == 0) return;
+
         int c = a+b;
         System.out.println(c);
         fibo(b, c, n-1);
