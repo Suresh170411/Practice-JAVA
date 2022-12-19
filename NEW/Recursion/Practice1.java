58a78fd8965b8f1a18a6505c2adf096b2381af58
suresh170411 <suresh.20fk@gmail.com>
2022-12-19 20:01:30 +0530

    recursion practice java

--- NEW/Recursion/Practice1.java
+++ NEW/Recursion/Practice1.java
@@ -4,4 +4,10 @@ public class Practice1 {
     public static void main(String[] args) {
         
     }
+    public static void calcFactorialBF(int n){
+        int sum = 1;
+        for (int i=1; i<=n; i++){
+            
+        }
+    }
 }
