8cb1ce0a910cadb76de512f85e526adcff3cd622
suresh170411 <suresh.20fk@gmail.com>
2022-12-19 20:29:23 +0530

    print number using recursion

--- NEW/Recursion/Practice3.java
+++ NEW/Recursion/Practice3.java
@@ -2,9 +2,12 @@ package NEW.Recursion;
 
 public class Practice3 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1;
+        printNumber(n);
     }
     public static void printNumber(int n){
-        
+        if (n == 6) return;
+        System.out.print(n+" ");
+        printNumber(n+1);
     }
 }
