5db9adee98015949e48a00305270aaabab23ac1d
suresh170411 <suresh.20fk@gmail.com>
2022-12-19 20:32:11 +0530

    print number using recursion

--- NEW/Recursion/Practice4.java
+++ NEW/Recursion/Practice4.java
@@ -3,7 +3,7 @@ package NEW.Recursion;
 public class Practice4 {
     public static void main(String[] args) {
         int n = 1;
-
+        printNums(n);
     }
     public static void printNums(int n){
         if (n == 6){
