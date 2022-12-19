be004866c0a5d5973a7f6b27aa2be6ef34ae5eb7
suresh170411 <suresh.20fk@gmail.com>
2022-12-19 22:35:30 +0530

    x to the power n using recursion

---
+++ NEW/Recursion/Practice7.java
@@ -0,0 +1,10 @@
+package NEW.Recursion;
+
+public class Practice7 {
+    public static void main(String[] args) {
+        int n = 2;
+        int x = 10;
+
+        System.out.println((int)Math.pow(x,n));
+    }
+}
