6df3c8b0acd0c17fce0c75d723a804df697a105a
suresh170411 <suresh.20fk@gmail.com>
2022-12-19 20:05:48 +0530

    recursion practice java

--- NEW/Recursion/Practice1.java
+++ NEW/Recursion/Practice1.java
@@ -3,13 +3,7 @@ package NEW.Recursion;
 public class Practice1 {
     public static void main(String[] args) {
         int n = 5;
-        calcFactorialBF(n);
+
     }
-    public static void calcFactorialBF(int n){
-        int sum = 1;
-        for (int i=1; i<=n; i++){
-            sum = sum*n;
-        }
-        System.out.println(sum);
-    }
+    public static void 
 }
