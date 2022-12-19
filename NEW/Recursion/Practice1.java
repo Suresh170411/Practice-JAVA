45ee8c442b8b35e0cdaf05b98ecb8bc8048f861a
suresh170411 <suresh.20fk@gmail.com>
2022-12-19 20:07:46 +0530

    recursion practice java

--- NEW/Recursion/Practice1.java
+++ NEW/Recursion/Practice1.java
@@ -3,7 +3,15 @@ package NEW.Recursion;
 public class Practice1 {
     public static void main(String[] args) {
         int n = 5;
-
+        System.out.println(calcFactorial(n));
     }
-    public static void 
+    public static int calcFactorial(int n){
+        if (n == 0 || n == 1){
+            return 1;
+        }
+
+        int sum = calcFactorial(n)*calcFactorial(n-1);
+
+        return sum;
+    }
 }
