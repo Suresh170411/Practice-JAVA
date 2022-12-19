18690c542ec3dfc7d578939118034c76e7feb40b
suresh170411 <suresh.20fk@gmail.com>
2022-12-19 22:08:32 +0530

    printing fibonacci using recurssion

---
+++ NEW/Recursion/Practice6.java
@@ -0,0 +1,11 @@
+package NEW.Recursion;
+
+public class Practice6 {
+    public static void main(String[] args) {
+        
+    }
+    public static void fibo(int n){
+        if (n<=1) return;
+        
+    }
+}
