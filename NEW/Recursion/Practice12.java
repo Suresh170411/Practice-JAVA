4237a902d7c4353be94d386c360892f8efdbc25e
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 17:56:49 +0530

    recursion practice java

--- NEW/Recursion/Practice12.java
+++ NEW/Recursion/Practice12.java
@@ -4,10 +4,12 @@ public class Practice12 {
     public static void main(String[] args) {
         System.out.println(hani(4));
     }
+
     public static int hani(int x){
         if (x < 1) return 1;
         else return hani(x-1) + bani(x);
     }
+    
     public static int bani(int x){
         if (x < 2) return 1;
         else return hani(x-1)+(x/2);
