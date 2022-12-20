526ecc56038e60990651cf04ea56ed65efbb38c1
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 10:47:33 +0530

    print factorial

--- NEW/Recursion/Practice9.java
+++ NEW/Recursion/Practice9.java
@@ -3,7 +3,7 @@ package NEW.Recursion;
 public class Practice9 {
     public static void main(String[] args) {
         
-        int n = 5;
+        int n = 0;
         printFacto(n);
 
     }
@@ -13,5 +13,12 @@ public class Practice9 {
             sum*= i;
         }
         System.out.println(sum);
+
+        System.out.println(printFactorial(n));
+    }
+
+    public static int printFactorial(int n){
+        if (n <= 1) return 1;
+        return n*printFactorial(n-1);
     }
 }
