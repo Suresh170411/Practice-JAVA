4750d9c6bd94b0ed473517d125b1be19e28ad913
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 08:27:45 +0530

    print x to the power n using recursion

--- NEW/Recursion/Practice7.java
+++ NEW/Recursion/Practice7.java
@@ -2,7 +2,7 @@ package NEW.Recursion;
 
 public class Practice7 {
     public static void main(String[] args) {
-        int x = 3;
+        int x = 5;
         int n = 2;
 
         int res = calcPower(x, n);
@@ -13,6 +13,6 @@ public class Practice7 {
         if (x == 0) return 0;
         if (n == 0) return 1;
 
-        
+        return x * calcPower(x, n-1);
     }
 }
