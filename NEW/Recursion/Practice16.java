4f5800a80751f8af7604d31ec607717c732425ac
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 21:41:35 +0530

    Tower of hanoi recursion probelm

---
+++ NEW/Recursion/Practice16.java
@@ -0,0 +1,8 @@
+package NEW.Recursion;
+
+public class Practice16 {
+    public static void main(String[] args) {
+        int n = 5;
+        
+    }
+}
--- NEW/Recursion/TowerOfHanoi.java
+++ NEW/Recursion/TowerOfHanoi.java
@@ -3,9 +3,10 @@ package NEW.Recursion;
 public class TowerOfHanoi {
     public static int count=0;
     public static void main(String[] args) {
-        int n = 3;
+        int n = 7;
         TOH(n, "SOURCE", "HELPER", "DESTINATION");
         System.out.println("Number of operations required : "+count);
+        System.out.println((int)Math.pow(2, n)-1);
     }
     public static void TOH(int n, String source, String helper, String destination){
         count++;
