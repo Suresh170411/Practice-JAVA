31591efbf3ec49e523e485cf08dd8d3245adc52b
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 11:03:24 +0530

    print nums recursion

--- NEW/Recursion/Practice10.java
+++ NEW/Recursion/Practice10.java
@@ -4,12 +4,21 @@ public class Practice10 {
     public static void main(String[] args) {
         int n = 5;
         printNums(n);
+        System.out.println();
+        printNumsRev(1);
     }
     public static void printNums(int n){
         if (n == 0){
             return;
         }
         printNums(n-1);
+        System.out.print(n+" ");
+    }
+    public static void printNumsRev(int n){
+        if (n > 5){
+            return;
+        }
+        printNumsRev(n+1);
         System.out.print(n+" ");
     }
 }
