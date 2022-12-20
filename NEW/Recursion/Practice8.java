5f3551087f662da1afe2f94b92afab14d1490adb
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 08:39:38 +0530

    calc pow x to the n logn

--- NEW/Recursion/Practice8.java
+++ NEW/Recursion/Practice8.java
@@ -2,6 +2,10 @@ package NEW.Recursion;
 
 public class Practice8 {
     public static void main(String[] args) {
+        int x = 3, n = 2;
+        
+    }
+    public static int calcPow(int x, int n){
         
     }
 }
