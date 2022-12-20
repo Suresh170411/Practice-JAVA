e92eb484ef8f50f43f906cc59b3902494e3e4a9b
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 20:10:18 +0530

    recursion for calculating GCD

--- NEW/Recursion/Practice15.java
+++ NEW/Recursion/Practice15.java
@@ -4,6 +4,8 @@ public class Practice15 {
     public static void main(String[] args) {
         System.out.println(GCD(60, 36));
     }
+
+    // RECURSIVE program for calculation GCD--> Greatest Common Index (Highest Common Factor)
     public static int GCD(int a, int b){
         if (a == 0){
             return b;
