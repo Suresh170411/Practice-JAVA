f730625632d040df010801bcba1a9e1096f5b0d2
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 17:49:18 +0530

    practice recursion

---
+++ NEW/Recursion/Practice11.java
@@ -0,0 +1,13 @@
+public class Practice11 {
+    public static void main(String[] args) {
+        System.out.println(fun(4, 3));
+    }
+    public static int fun(int a, int b){
+        if (b == 0) return 0;
+        if (b %2 == 0){
+            return fun(a+a, b/2);
+        }else {
+            return fun(a+a, b/2)+a;
+        }
+    }
+}
