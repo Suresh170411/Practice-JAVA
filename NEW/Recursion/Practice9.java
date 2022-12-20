cb0d320ca3920c029d3a080d8462124e3203389c
suresh170411 <suresh.20fk@gmail.com>
2022-12-20 10:43:36 +0530

    recursion using normal for loop

---
+++ NEW/Recursion/Practice9.java
@@ -0,0 +1,12 @@
+package NEW.Recursion;
+
+public class Practice9 {
+    public static void main(String[] args) {
+        int n = 5;
+        int sum = 1;
+        for (int i=1; i<=5; i++){
+            sum = sum*i;
+        }
+        System.out.println(sum);
+    }
+}
