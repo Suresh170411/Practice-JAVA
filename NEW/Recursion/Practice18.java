7b263d647deb4c5ef7eaf7a03262859fda68e6ff
suresh170411 <suresh.20fk@gmail.com>
2022-12-22 09:19:22 +0530

    element at end practice recursion

--- NEW/Recursion/Practice18.java
+++ NEW/Recursion/Practice18.java
@@ -2,10 +2,27 @@ package NEW.Recursion;
 
 public class Practice18 {
     public static void main(String[] args) {
+        String str = "abxgexixox";
         
     }
 
     public static void elementAtEndBF(String str){
-        
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i) == 'x'){
+                count++;
+                continue;
+            }else {
+                sb.append(str.charAt(i));
+            }
+        }
+        // System.out.println(sb);
+
+        for (int i=0; i<count; i++){
+            sb.append('x');
+        }
+
+        System.out.println(sb.toString());
     }
 }
