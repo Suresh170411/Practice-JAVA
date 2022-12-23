ba1e2e6a5034097fb962a3586704ce6d6818c6d5
suresh170411 <suresh.20fk@gmail.com>
2022-12-23 17:39:27 +0530

    recursion practice java

---
+++ NEW/Recursion/Practice19.java
@@ -0,0 +1,9 @@
+package NEW.Recursion;
+
+public class Practice19 {
+    public static void main(String[] args) {
+        String str = "abc ";
+        String bag = " ";
+        System.out.println(str); 
+    }
+}
