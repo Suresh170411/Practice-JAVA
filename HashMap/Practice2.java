ea784afeb9942f1127a2dda47f9995e7e25267f6
suresh170411 <suresh.20fk@gmail.com>
2022-12-29 21:37:22 +0530

    hashmap practice

--- HashMap/Practice2.java
+++ HashMap/Practice2.java
@@ -1,8 +1,19 @@
 package HashMap;
 
+import java.util.HashMap;
+
 public class Practice2 {
     public static void main(String[] args) {
         int arr [] = {2,4,1,0,5,7};
-        
+
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        for (int i=0; i<arr.length; i++){
+            if (hm.containsKey(arr[i])){
+                hm.put(arr[i], 1);
+            }else {
+                
+            }
+        }
     }
 }
