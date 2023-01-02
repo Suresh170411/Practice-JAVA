0ac53572d65612d1496cbe4a7b994684f05cde00
suresh170411 <suresh.20fk@gmail.com>
2023-01-02 10:19:53 +0530

    stack java

--- Stack/StackProb.java
+++ Stack/StackProb.java
@@ -6,7 +6,7 @@ public class StackProb {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
 
-        stack.push(10); 
+        stack.push(10);
         stack.push(20);
         stack.push(30);
         stack.push(40);
