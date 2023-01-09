0288f0501c8a0db5f1569baeda060aac47dd7ef1
suresh170411 <suresh.20fk@gmail.com>
2023-01-09 17:46:53 +0530

    remove dups from string

--- RatanSir/StringDups.java
+++ RatanSir/StringDups.java
@@ -1,6 +1,7 @@
 package RatanSir;
 
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 
 public class StringDups {
     public static void main(String[] args) {
@@ -12,5 +13,14 @@ public class StringDups {
         char [] arr = str.toCharArray();
 
         System.out.println(Arrays.toString(arr));
+
+        LinkedHashSet<Character> lhs =  new LinkedHashSet<>();
+
+        for (int i=0; i<arr.length; i++){
+            lhs.add(arr[i]);
+        }
+
+        System.out.println(lhs);
+
     }
 }
