c04fd87e72287f42051a1694eea8b465525b394d
suresh170411 <suresh.20fk@gmail.com>
2023-01-09 19:45:18 +0530

    arrys equals method for check the arrays

---
+++ Arrays/ArrayComp.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArrayComp {
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3,4,5};
+        int [] arr2 = {1,2,3,4,5};
+
+        boolean res = Arrays.equals(arr1, arr2);
+        System.out.println(res);
+    }
+}
