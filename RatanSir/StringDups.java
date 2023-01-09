40d680f4149bc4c2eabfe74c7c330e80dd5684ed
suresh170411 <suresh.20fk@gmail.com>
2023-01-09 17:36:48 +0530

    string tochar array check java

--- RatanSir/StringDups.java
+++ RatanSir/StringDups.java
@@ -1,5 +1,9 @@
 package RatanSir;
 
 public class StringDups {
-    
+    public static void main(String[] args) {
+        String str = "HEllo";
+
+        char [] arr = new str.toCharArray();
+    }
 }
