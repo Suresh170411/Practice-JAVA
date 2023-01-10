41fa962e6ff7a7b9807110989708312d3a044035
suresh170411 <suresh.20fk@gmail.com>
2023-01-10 09:49:27 +0530

    comparator check practice java

--- Arrays/ComparatorCheck.java
+++ Arrays/ComparatorCheck.java
@@ -1,9 +1,6 @@
 package Arrays;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.TreeSet;
 
 public class ComparatorCheck implements Comparator<Student> {
