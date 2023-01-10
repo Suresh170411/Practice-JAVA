efdde34e0041971f578d9cdd3f0a21dd16b86e5d
suresh170411 <suresh.20fk@gmail.com>
2023-01-10 10:39:13 +0530

    comparator

--- Comparoator/ListSort.java
+++ Comparoator/ListSort.java
@@ -1,5 +1,26 @@
 package Comparoator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import Arrays.Student;
+
 public class ListSort {
-    
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+
+        list.add(new Student(10, "Suresh", 650));
+        list.add(new Student(11, "Suresh", 350));
+        list.add(new Student(12, "Suresh", 550));
+
+
+        // Collections.sort(list, new Comparator<Student>() {
+        //     @Override
+        //     public int compare(Student u1, Student u2) {
+        //       return u1.getMarks().equals(u2.getMarks());
+        //     }
+        //   });
+    }
 }
