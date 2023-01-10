26cf8751f7e41f27fc72d0ea83894f556ad59531
suresh170411 <suresh.20fk@gmail.com>
2023-01-10 09:32:10 +0530

    comparable interface practice

--- Arrays/ComparatorCheck.java
+++ Arrays/ComparatorCheck.java
@@ -1,5 +1,12 @@
 package Arrays;
 
-public class ComparatorCheck {
-    
+import java.util.Comparator;
+
+public class ComparatorCheck implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        
+        return o1.getMarks() > o2.getMarks() ? +1 : -1;
+    }
 }
